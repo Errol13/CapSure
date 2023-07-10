c553bf823152d99ed5ba2282ab07086ea571371d
DEPED <ecelis@gbox.adnu.edu.ph>
2023-07-10 19:21:33 +0800

    db cutie

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Start the Cloud SQL Proxy
+/usr/local/bin/cloud_sql_proxy -instances=your-project-id:your-region:your-instance-connection-name=tcp:3306 &
+
+# Wait for the Cloud SQL Proxy to start
+sleep 5
+
 echo executing sed
 sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 
