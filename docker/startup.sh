86a17dd11564398169e4ab47601a13f570147057
DEPED <ecelis@gbox.adnu.edu.ph>
2023-07-10 19:22:59 +0800

    db cutie

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start the Cloud SQL Proxy
-/usr/local/bin/cloud_sql_proxy -instances=your-project-id:your-region:your-instance-connection-name=tcp:3306 &
+/usr/local/bin/cloud_sql_proxy -instances=team-tem:asia-southeast1:capsuredb=tcp:3306 &
 
 # Wait for the Cloud SQL Proxy to start
 sleep 5
